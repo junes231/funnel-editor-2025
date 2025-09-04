48082bfa39648fc02931e68261254fa575cdcaa6
junes231 <deyongw3@gmail.com>
2025-09-04 15:04:25 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 // 只需导入一次，内部已经防重复
 import { db, auth } from './firebase.ts';
-import { installlongPressDebug } from './utils/longPressDebug.ts';
+import { installlongPressDebug } from './utils/longPressDebug';
 
 // 初始化长按调试
 installlongPressDebug({
