6202fc4e07b44f792047c6de3cf00e153975534d
junes231 <deyongw3@gmail.com>
2025-09-10 14:44:35 +0800

    Add SomePage component with BackButton links

---
+++ src/pages/SomePage.js
@@ -0,0 +1,13 @@
+import BackButton from "../components/BackButton";
+
+function SomePage() {
+  return (
+    <div>
+      <BackButton>&lt; Back to All Funnels</BackButton>
+      <BackButton>Back to Funnel Dashboard</BackButton>
+      <BackButton>&lt; Back to Editor</BackButton>
+    </div>
+  );
+}
+
+export default SomePage;
