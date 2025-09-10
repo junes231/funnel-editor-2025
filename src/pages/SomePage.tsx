8bd998723773b5dab941edf3e4df752c08eae1b3
junes231 <deyongw3@gmail.com>
2025-09-10 17:18:10 +0800

    Update SomePage.tsx

--- src/pages/SomePage.tsx
+++ src/pages/SomePage.tsx
@@ -1,11 +1,17 @@
+// src/pages/SomePage.tsx
 import BackButton from "../components/BackButton.tsx";
 
 function SomePage() {
   return (
     <div>
-      <BackButton>&lt; Back to All Funnels</BackButton>
-      <BackButton>Back to Funnel Dashboard</BackButton>
-      <BackButton>&lt; Back to Editor</BackButton>
+      {/* 假设“/”是所有漏斗的列表页 */}
+      <BackButton to="/">&lt; Back to All Funnels</BackButton>
+
+      {/* 假设“/dashboard”是漏斗的仪表盘 */}
+      <BackButton to="/dashboard">Back to Funnel Dashboard</BackButton>
+
+      {/* 假设“/editor”是编辑器页面 */}
+      <BackButton to="/editor">&lt; Back to Editor</BackButton>
     </div>
   );
 }
