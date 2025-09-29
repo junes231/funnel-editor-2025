c253dcab790d1c69090081fb4a851e44ca21e3a2
junes231 <deyongw3@gmail.com>
2025-09-29 15:55:41 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,13 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'react-app',
-    'plugin:@typescript-eslint/recommended'
-  ],
-  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: ['react-app', 'plugin:react-hooks/recommended'],
   rules: {
     'no-unused-vars': 'warn',
-    '@typescript-eslint/no-unused-vars': 'warn',
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
+    'react-hooks/exhaustive-deps': 'warn',
   },
   env: {
     browser: true,
     es2021: true,
-    jest: true
-  }
+    jest: true,
+  },
 };
