99c841f0fae3fe35c9b4b3407f9fb224977dbfcc
junes231 <deyongw3@gmail.com>
2025-10-08 16:49:01 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback, useRef, ChangeEvent } from 'react';
 import { getAuth, onAuthStateChanged, User, signOut } from 'firebase/auth';
+import { FunnelData, FunnelComponent, Answer, Question } from './types/funnel.ts'; 
 import debounce from 'lodash.debounce'; 
 import QuizPlayer from './components/QuizPlayer.tsx';
 import ResetPage from './pages/reset.tsx';
@@ -28,12 +29,7 @@ import {
 import Login from './components/Login.tsx';
 import './App.css';
 
-// --- Interface Definitions ---
-interface Answer {
-  id: string;
-  text: string;
-  clickCount?: number;
-}
+
 
 interface Question {
   id: string;
