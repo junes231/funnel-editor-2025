4dbb3dbad8886dcef24a4d9f14bf652d6b05bc7c
junes231 <deyongw3@gmail.com>
2025-10-12 16:52:13 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1214,14 +1214,15 @@ const QuizEditorComponent: React.FC<QuizEditorComponentProps> = ({
            className="question-item" 
               onClick={() => onEditQuestion(index)}
          >
-           <div class="question-header-container"> 
-                  <span class="question-badge">Q{index + 1}</span>
-                  <span class="question-id-text">(ID: {q.id})</span>
+           <div style={{width: '100%', marginBottom: '10px'}}>
+                
               </div>
               
-              {/* ✅ 2. 问题标题作为单独的 Flex Item，让 line-height 自由发挥 */}
-              <span class="question-title-text">{q.title}</span> 
+            <span className="question-title-text">{q.title}</span> 
+              
+           <span className="question-id-text">(ID: {q.id})</span>
 
+           <span class="question-badge">Q{index + 1}</span>
             </li>
           ))}
         </ul>
